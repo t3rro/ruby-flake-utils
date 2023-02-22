9e032374b266af6802db48de7f3c6bdf109e715f
t3rro <t3rro@protonmail.com>
2023-02-22 08:28:54 -0800

    init

--- flake.nix
+++ flake.nix
@@ -3,27 +3,27 @@
 
   outputs = { ... }:
     let
-      funcs = {
+      mkFuncs = pkgs: {
         mkRubyScript =
           # take script and dispatch it with the local bundle binary
           script: pkgs.writeShellScriptBin script "${bins.bundle} exec ${script} $@";
       };
 
-      scripts = {
-        rake = lib.gemDefaults.funcs.mkRubyScript "rake";
+      mkScripts = funcs: {
+        rake = funcs.mkRubyScript "rake";
         rubyDevScripts = [ scripts.rake ];
       };
 
-      envs = {
+      mkEnvs = pkgs: configurations: {
         gems = pkgs.bundlerEnv configurations.bundlerConfig;
       };
 
-      bins = {
+      mkBins = envs: {
         ruby = pkgs.ruby_3_1;
         bundle = "${envs.gems}/bin/bundle";
       };
 
-      mkConfiguration = name: pkgs:
+      mkConfigurations = name: pkgs:
         {
 
           bundlerConfig = {
@@ -55,33 +55,11 @@
       # use lib keyword on outputs to expose nix functions
       lib = {
         mkGemDefaults = { name, pkgs }: {
-          configurations = mkConfiguration name pkgs;
-          # configurations = {
-          #
-          #   bundlerConfig = {
-          #     inherit name;
-          #     ruby = bins.ruby;
-          #     lockfile = ./Gemfile.lock;
-          #     gemfile = ./Gemfile;
-          #     gemset = ./gemset.nix;
-          #   };
-          #
-          #   derivationConfig = {
-          #     src = ./.;
-          #     inherit name;
-          #     buildInputs = [
-          #       bins.ruby
-          #       envs.gems
-          #       pkgs.makeWrapper
-          #       pkgs.git
-          #     ] ++ scripts.rubyDevScripts;
-          #     installPhase = ''
-          #       mkdir -p $out/{bin,share/${name}}
-          #       cp -r * $out/share/${name}
-          #     '';
-          #
-          #   };
-          # };
+          configurations = mkConfigurations name pkgs;
+          funcs = mkFuncs pkgs;
+          scripts = mkScripts funcs;
+          envs = mkEnvs pkgs configurations;
+          envs = mkBins pkgs configurations;
         };
       };
     };
