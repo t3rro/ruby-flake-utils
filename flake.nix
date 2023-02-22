2a5f077b36c4732c1974106207ae6df54599219d
t3rro <t3rro@protonmail.com>
2023-02-22 08:09:43 -0800

    init

--- flake.nix
+++ flake.nix
@@ -3,32 +3,32 @@
 
   outputs = rec {
     lib = {
-      gemDefaults = { name, pkgs }: {
+      mkGemDefaults = { name, pkgs }: {
         funcs = {
           mkRubyScript =
             # take script and dispatch it with the local bundle binary
-            script: pkgs.writeShellScriptBin script "${gemDefaults.bins.bundle} exec ${script} $@";
+            script: pkgs.writeShellScriptBin script "${lib.gemDefaults.bins.bundle} exec ${script} $@";
         };
 
         scripts = {
-          rake = outputs.lib.gemDefaults.funcs.mkRubyScript "rake";
-          rubyDevScripts = [ outputs.lib.gemDefaults.scripts.rake ];
+          rake = lib.gemDefaults.funcs.mkRubyScript "rake";
+          rubyDevScripts = [ lib.gemDefaults.scripts.rake ];
         };
 
         envs = {
-          gems = pkgs.bundlerEnv configurations.bundlerConfig;
+          gems = pkgs.bundlerEnv lib.gemDefaults.configurations.bundlerConfig;
         };
 
         bins = {
           ruby = pkgs.ruby_3_1;
-          bundle = "${outputs.lib.gemDefaults.envs.gems}/bin/bundle";
+          bundle = "${lib.gemDefaults.envs.gems}/bin/bundle";
         };
 
         configurations = {
 
           bundlerConfig = {
             inherit name;
-            ruby = outputs.lib.gemDefaults.bins.ruby;
+            ruby = lib.gemDefaults.bins.ruby;
             lockfile = ./Gemfile.lock;
             gemfile = ./Gemfile;
             gemset = ./gemset.nix;
@@ -38,11 +38,11 @@
             src = ./.;
             inherit name;
             buildInputs = [
-              outputs.lib.gemDefaults.bins.ruby
-              outputs.lib.gemDefaults.envs.gems
+              lib.gemDefaults.bins.ruby
+              lib.gemDefaults.envs.gems
               pkgs.makeWrapper
               pkgs.git
-            ] ++ outputs.lib.gemDefaults.scripts.rubyDevScripts;
+            ] ++ lib.gemDefaults.scripts.rubyDevScripts;
             installPhase = ''
               mkdir -p $out/{bin,share/${name}}
               cp -r * $out/share/${name}
