2015010d3d760c53a1ec61eeb4c584f00d0981dd
t3rro <t3rro@protonmail.com>
2023-02-22 07:59:01 -0800

    init

--- flake.nix
+++ flake.nix
@@ -1,7 +1,56 @@
 {
   description = "produce ruby flakes";
 
-  outputs = {
-    lib = { };
+  outputs = rec {
+    lib = {
+      gemDefaults = { name, pkgs }: {
+        funcs = {
+          mkRubyScript =
+            # take script and dispatch it with the local bundle binary
+            script: pkgs.writeShellScriptBin script "${gemDefaults.bins.bundle} exec ${script} $@";
+        };
+
+        scripts = {
+          rake = outputs.lib.gemDefaults.funcs.mkRubyScript "rake";
+          rubyDevScripts = [ outputs.lib.gemDefaults.scripts.rake ];
+        };
+
+        envs = {
+          gems = pkgs.bundlerEnv configurations.bundlerConfig;
+        };
+
+        bins = {
+          ruby = pkgs.ruby_3_1;
+          bundle = "${outputs.lib.gemDefaults.envs.gems}/bin/bundle";
+        };
+
+        configurations = {
+
+          bundlerConfig = {
+            inherit name;
+            ruby = outputs.lib.gemDefaults.bins.ruby;
+            lockfile = ./Gemfile.lock;
+            gemfile = ./Gemfile;
+            gemset = ./gemset.nix;
+          };
+
+          derivationConfig = {
+            src = ./.;
+            inherit name;
+            buildInputs = [
+              outputs.lib.gemDefaults.bins.ruby
+              outputs.lib.gemDefaults.envs.gems
+              pkgs.makeWrapper
+              pkgs.git
+            ] ++ outputs.lib.gemDefaults.scripts.rubyDevScripts;
+            installPhase = ''
+              mkdir -p $out/{bin,share/${name}}
+              cp -r * $out/share/${name}
+            '';
+
+          };
+        };
+      };
+    };
   };
 }
