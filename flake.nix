ed5bab28574350fac594044773e814c58183d1b4
t3rro <t3rro@protonmail.com>
2023-02-23 13:39:20 -0800

    update lock

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       # mkGemSystems = name: nixpkgs: lockfile: gemfile: gemset: strategy: src:
       #   flake-utils.lib.eachDefaultSystem
       #     (system: mkGemSystem system name nixpkgs lockfile gemfile gemset strategy src);
-      mkGemSystems = { system, name, nixpkgs, lockfile, gemfile, gemset, strategy, src, ... }:
+      mkGemSystems = { name, nixpkgs, lockfile, gemfile, gemset, strategy, src, ... }:
         flake-utils.lib.eachDefaultSystem (system: mkGemSystem { inherit system name nixpkgs lockfile gemfile gemset strategy src; });
 
       # understanding that flake-utils.lib.eachDefaultSystem creates a system
